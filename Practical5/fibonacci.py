d1f4889ab1aee982a33a3f26a48a6a83bb056950
3200110843bit <haonanl.20@intl.zju.edu.cn>
2021-03-10 14:26:17 +0800

    edit fibonacci.py

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,27 @@
+a=1
+b=1
+a=a+b
+b=b+a
+a=a+b
+b=b+a
+a=a+b
+b=b+a
+a=a+b
+b=b+a
+a=a+b
+b=b+a
+a=a+b
+print a
+
+X=1
+Y=1
+print X
+print Y
+for i in range(1,6):
+  X= X+Y
+  Y= Y+X
+  print X
+  print Y
+X= X+Y
+print X
+
